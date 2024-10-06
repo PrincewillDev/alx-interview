602db4efa9582ff416b822cfc49ebcf1bca7c52b
PrincewillDev <princewillfidelis1@gmail.com>
2024-10-06 13:52:10 +0100

    Fixed pycodestyle error

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -6,7 +6,7 @@ def pascal_triangle(n):
     for i in range(n - 1):
         temp = [0] + result[-1] + [0]
         row = []
-        
+
         for j in range(len(result[-1]) + 1):
             row.append(temp[j] + temp[j + 1])
         result.append(row)
